2f989d4d850e7ba37cff4cf90af601b25cd26516
Nopparat011 <152350874+Nopparat011@users.noreply.github.com>
2024-08-01 14:20:44 +0700

    Create ExNo2.java

---
+++ ExNo2.java
@@ -0,0 +1,12 @@
+public class ExNo2 {
+    public static void main(String[] args) {
+        int upperLimit = 100;
+        System.out.println("Numbers that are divisible by 3 and 5 (Not more than " + upperLimit + "):");
+        for (int i = 1; i <= upperLimit; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i);
+            }
+        }  
+     }
+ }
+
