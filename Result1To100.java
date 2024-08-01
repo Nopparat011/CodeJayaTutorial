9c86cac5aa678f5dac315dc6c95531a310133b66
Nopparat011 <152350874+Nopparat011@users.noreply.github.com>
2024-08-01 10:25:01 +0700

    Update Result1To100.java

--- Result1To100.java
+++ Result1To100.java
@@ -1,14 +1,11 @@
 public class Result1To100 {
     public static void main(String[] args) {
-        int sum = 0;
-        int i = 1;
-
+        int i = 1, sum = 0;
         while (i <= 100) {
             sum += i;
-        System.out.println(" i: " + i + ", Result in whileLoop : " + sum);
             i++;
         }
-        System.out.println(" Result in whileLoop 1 - 100 is : " + sum);
+        System.out.println(" Result in whileLoop 1 To 100  : " + sum);
     }
 }
 
