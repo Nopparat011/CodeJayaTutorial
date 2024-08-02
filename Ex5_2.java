e2f2537b94ff140c688a6ce74297b162f821d2fc
Nopparat011 <152350874+Nopparat011@users.noreply.github.com>
2024-08-02 14:57:35 +0700

    Create Ex5_2.java

---
+++ Ex5_2.java
@@ -0,0 +1,9 @@
+public class Ex5_2 {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 10; x++) {
+            int result = x * x * x;
+            System.out.println(x + "^3 = " + result);
+        }
+    }
+}
+
